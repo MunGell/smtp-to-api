08f40feedfb678fcfb5fc48d3a32141940eb2bac
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2017-03-06 14:52:52 +0000

    Add pytest configuration and remove commented code

--- smtp_to_api/__init__.py
+++ smtp_to_api/__init__.py
@@ -13,37 +13,3 @@ if __name__ == '__main__':
         loop.run_forever()
     except KeyboardInterrupt:
         pass
-
-# import smtpd
-# import asyncore
-#
-# import smtp_to_api.config
-#
-# server = smtpd.SMTPServer((SMTP_HOST, SMTP_PORT), None)
-#
-# asyncore.loop()
-
-# class CustomSMTPServer(smtpd.SMTPServer):
-#     def process_message(self, peer, mailfrom, rcpttos, data):
-#         print('Receiving message from:', peer)
-#         print('Message addressed from:', mailfrom)
-#         print('Message addressed to  :', rcpttos)
-#         print('Message length        :', len(data))
-#         return
-
-#
-# server = CustomSMTPServer(('127.0.0.1', 1025), None)
-#
-# asyncore.loop()
-
-# from flask import Flask, request
-#
-# app = Flask(__name__, instance_relative_config=True)
-# app.config.from_object('config')
-#
-#
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def index():
-#     print(request)
-#     exit()
